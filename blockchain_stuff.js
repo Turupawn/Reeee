c90392491c2aba053cf922122edb06b2247ac3f9
turupawn <ahmed.hn.43@gmail.com>
2022-04-04 16:29:52 -0600

    first commit

---
+++ blockchain_stuff.js
@@ -0,0 +1,124 @@
+const NETWORK_ID = 137
+
+const MY_CONTRACT_ADDRESS = "0xeB8ABaa4aD374900A21da7932bC07882875041E4"
+const MY_CONTRACT_ABI_PATH = "./json_abi/MyContract.json"
+var my_contract
+
+var accounts
+var web3
+
+function metamaskReloadCallback() {
+  window.ethereum.on('accountsChanged', (accounts) => {
+    document.getElementById("web3_message").textContent="Se cambió el account, refrescando...";
+    window.location.reload()
+  })
+  window.ethereum.on('networkChanged', (accounts) => {
+    document.getElementById("web3_message").textContent="Se el network, refrescando...";
+    window.location.reload()
+  })
+}
+
+const getWeb3 = async () => {
+  return new Promise((resolve, reject) => {
+    if(document.readyState=="complete")
+    {
+      if (window.ethereum) {
+        const web3 = new Web3(window.ethereum)
+        window.location.reload()
+        resolve(web3)
+      } else {
+        reject("must install MetaMask")
+        document.getElementById("web3_message").textContent="Error: Porfavor conéctate a Metamask";
+      }
+    }else
+    {
+      window.addEventListener("load", async () => {
+        if (window.ethereum) {
+          const web3 = new Web3(window.ethereum)
+          resolve(web3)
+        } else {
+          reject("must install MetaMask")
+          document.getElementById("web3_message").textContent="Error: Please install Metamask";
+        }
+      });
+    }
+  });
+};
+
+const getContract = async (web3, address, abi_path) => {
+  const response = await fetch(abi_path);
+  const data = await response.json();
+  
+  const netId = await web3.eth.net.getId();
+  contract = new web3.eth.Contract(
+    data,
+    address
+    );
+  return contract
+}
+
+async function loadDapp() {
+  metamaskReloadCallback()
+  document.getElementById("web3_message").textContent="Please connect to Metamask"
+  var awaitWeb3 = async function () {
+    web3 = await getWeb3()
+    web3.eth.net.getId((err, netId) => {
+      if (netId == NETWORK_ID) {
+        var awaitContract = async function () {
+          my_contract = await getContract(web3, MY_CONTRACT_ADDRESS, MY_CONTRACT_ABI_PATH)
+          document.getElementById("web3_message").textContent="You are connected to Metamask"
+          onContractInitCallback()
+          web3.eth.getAccounts(function(err, _accounts){
+            accounts = _accounts
+            if (err != null)
+            {
+              console.error("An error occurred: "+err)
+            } else if (accounts.length > 0)
+            {
+              onWalletConnected()
+              document.getElementById("account_address").style.display = "block"
+            } else
+            {
+              document.getElementById("connect_button").style.display = "block"
+            }
+          });
+        };
+        awaitContract();
+      } else {
+        document.getElementById("web3_message").textContent="Please connect to Polygon";
+      }
+    });
+  };
+  awaitWeb3();
+}
+
+async function connectWallet() {
+  await window.ethereum.request({ method: "eth_requestAccounts" })
+  accounts = await web3.eth.getAccounts()
+  onWalletConnected()
+}
+
+loadDapp()
+
+const onContractInitCallback = async () => {
+  console.log(await my_contract.methods.new_contract_address().call())
+}
+
+const onWalletConnected = async () => {
+}
+
+
+//// Minter ////
+
+const launchToken = async (factory_address, data) => {
+  const result = await my_contract.methods.launch(factory_address, data)
+  .send({ from: accounts[0], gas: 0, value: 0 })
+  .on('transactionHash', function(hash){
+    document.getElementById("web3_message").textContent="Creating token...";
+  })
+  .on('receipt', function(receipt){
+    document.getElementById("web3_message").textContent="Success.";    })
+  .catch((revertReason) => {
+    console.log("ERROR! Transaction reverted: " + revertReason.receipt.transactionHash)
+  });
+}
